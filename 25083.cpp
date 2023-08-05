26f79a8f4aeb74e66cc9228fa69222ae3d2fa119
gwlee <leemarshal@naver.com>
2023-08-06 00:46:25 +0900

    [Bronze V] Title: 새싹(#25083), Memory: 2020 KB, Time: 0 ms

---
+++ 25083.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cout << "         ,r'\"7\n"
+         << "r`-_   ,'  ,/\n"
+         << " \\. \". L_r'\n"
+         << "   `~\\/\n"
+         << "      |\n"
+         << "      |";
+    return 0;
+}
+//
+// Created by 규북 on 2023/08/06.
+//
