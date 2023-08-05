6d5c66bdbc0774273291de136b870864495906f8
gwlee <leemarshal@naver.com>
2023-08-06 01:23:37 +0900

    [Bronze V] Title: AxB, Memory: 2020 KB, Time: 0 ms

---
+++ 10998.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a, b;
+    cin >> a >> b;
+    cout << a * b;
+    return 0;
+}
+//
+// Created by 규북 on 2023/08/06.
+//
