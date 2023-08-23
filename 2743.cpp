bbc94aa98ba649019bbcaf409f1aef221d4eb8ee
gwlee <leemarshal@naver.com>
2023-08-23 23:21:55 +0800

    [Bronze V] Title: 단어 길이 재기(#2743), Memory: 2024 KB, Time: 0 ms

---
+++ 2743.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace::std;
+int main() {
+    string input;
+    cin >> input;
+    cout << input.length();
+    return 0;
+}
+//
+// Created by 규북 on 2023/08/23.
+//
